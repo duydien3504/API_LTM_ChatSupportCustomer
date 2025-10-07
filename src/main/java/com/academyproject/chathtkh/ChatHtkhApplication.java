10af9822d8b69c34bdec4f92eabef34432f5c953
duydien3504 <duydien3504@gmail.com>
2025-10-07 16:55:26 +0700

    Update file

---
+++ src/main/java/com/academyproject/chathtkh/ChatHtkhApplication.java
@@ -0,0 +1,13 @@
+package com.academyproject.chathtkh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatHtkhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatHtkhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/academyproject/chathtkh/ChatHtkhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academyproject.chathtkh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatHtkhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
